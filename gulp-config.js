9a98f1e4c03199c52834813b73b30f1d6375eca0
Patrick W Larsen <patrickwlarsen@gmail.com>
2017-11-29 13:14:51 +0100

    i might have dun goofed

---
+++ gulp-config.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\Sitecore8\\Website",
+        devRoot: "C:\\dev\\sitecore\\projects\\SitecoreDev",
+        solutionName: "SitecoreDev",
+        buildConfiguration: "Debug"
+    }
+    return config;
+}
