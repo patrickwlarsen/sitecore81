a68e928f3fb31bab1aefa485382c11968306ca3e
Patrick W Larsen <patrickwlarsen@gmail.com>
2017-12-01 13:13:03 +0100

    Incorporated Inversion of Control. In so doing, i broke something.

---
+++ Foundation/Repository/SitecoreDev.Foundation.Repository/Pipelines/InitializeContainer/RegisterDependencies.cs
@@ -0,0 +1,13 @@
+﻿using SitecoreDev.Foundation.Ioc.Pipelines.InitializeContainer;
+using SitecoreDev.Foundation.Repository.Content;
+
+namespace SitecoreDev.Foundation.Repository.Pipelines.InitializeContainer
+{
+    public class RegisterDependencies
+    {
+        public void Process(InitializeContainerArgs args)
+        {
+            args.Container.Register<IContentRepository, SitecoreContentRepository>();
+        }
+    }
+}
