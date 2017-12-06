6de1199848aa79e97c81704422a93150e255eaf0
Patrick W Larsen <patrickwlarsen@gmail.com>
2017-12-06 12:57:12 +0100

    CSS and JS minification with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require("gulp");
+var cssmin = require("gulp-cssmin");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 var msbuild = require("gulp-msbuild");
 var debug = require("gulp-debug");
 var foreach = require("gulp-foreach");
@@ -25,4 +29,64 @@ gulp.task("Publish-Site", function () {
                     toolsVersion: 15.0
                 }));
         }));
+});
+
+var minifyCss = function (destination) {
+    gulp.src("./{Feature,Foundation,Project}/**/**/Content/*.css")
+        .pipe(concat('sitecoredev.website.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(destination));
+};
+
+var minifyJs = function (destination) {
+    gulp.src("./{Feature,Foundation,Project}/**/**/Scripts/*.js")
+        .pipe(concat('sitecoredev.website.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(destination));
+};
+
+gulp.task("minify-css", function () {
+    minifyCss();
+});
+
+gulp.task("minify-js", function () {
+    minifyJs();
+});
+
+gulp.task("css-watcher", function () {
+    var root = "./";
+    var roots = [root + "**/Content", "!" + root + "/**/obj/**/Content"];
+    var files = "/**/*.css";
+    var destination = gulpConfig.webRoot + "\\Content";
+    gulp.src(roots, { base: root }).pipe(
+        foreach(function (stream, rootFolder) {
+            gulp.watch(rootFolder.path + files, function (event) {
+                if (event.type === "changed") {
+                    console.log("publish this file " + event.path);
+                    minifyCss(destination);
+                }
+                console.log("published " + event.path);
+            });
+            return stream;
+        })
+    );
+});
+
+gulp.task("js-watcher", function () {
+    var root = "./";
+    var roots = [root + "**/Scripts", "!" + root + "/**/obj/**/Scripts"];
+    var files = "/**/*.js";
+    var destination = gulpConfig.webRoot + "\\Scripts";
+    gulp.src(roots, { base: root }).pipe(
+        foreach(function (stream, rootFolder) {
+            gulp.watch(rootFolder.path + files, function (event) {
+                if (event.type === "changed") {
+                    console.log("publish this file " + event.path);
+                    minifyJs(destination);
+                }
+                console.log("published " + event.path);
+            });
+            return stream;
+        })
+    );
 });
