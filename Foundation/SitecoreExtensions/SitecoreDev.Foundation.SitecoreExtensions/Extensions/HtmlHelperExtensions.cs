15047dc5b346b069784e06c97ca091132dae5d19
Patrick W Larsen <patrickwlarsen@gmail.com>
2017-12-06 09:47:19 +0100

    Customizing Renderings In the Experience Editor

---
+++ Foundation/SitecoreExtensions/SitecoreDev.Foundation.SitecoreExtensions/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using DynamicPlaceholders.Mvc.Extensions;
+using Sitecore.Mvc.Helpers;
+
+namespace SitecoreDev.Foundation.SitecoreExtensions.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString DynamicPlaceholder(this SitecoreHelper helper, string placeholderName)
+        {
+            return SitecoreHelperExtensions.DynamicPlaceholder(helper, placeholderName);
+        }
+    }
+}
